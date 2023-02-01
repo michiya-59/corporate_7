de3cfadd4f9fad462d8e7777c9490817e6a56044
michiya-59 <nishino.michiya0509@gmail.com>
2023-02-01 09:45:23 +0900

    微調整

--- js/hamburger.js
+++ js/hamburger.js
@@ -5,19 +5,42 @@ window.addEventListener('load', () => {
   let header = document.getElementById('header');
   let hunburger_menu = document.getElementById('hunburger_menu');
   let mask_menu = document.getElementById('mask_menu');
+  let nav_menu = document.getElementById('nav_menu');
+  let pickup = document.getElementById('pickup');
+  let contact = document.getElementById('contact');
 
   hunburger_menu.addEventListener('click', () => {
     header.classList.toggle('open_header');
     hunburger_menu.classList.toggle('open');
-  });
-  
-  hunburger_menu.addEventListener('click', () => {
     mask_menu.classList.toggle('open_mask');
+    nav_menu.classList.toggle('opan_nav_menu');
   });
   
   mask_menu.addEventListener('click', () => {
     header.classList.remove('open_header');
     hunburger_menu.classList.remove('open');
     mask_menu.classList.remove('open_mask');
+    nav_menu.classList.remove('opan_nav_menu');
   });
+  
+  nav_menu.addEventListener('click', () => {
+    header.classList.remove('open_header');
+    hunburger_menu.classList.remove('open');
+    mask_menu.classList.remove('open_mask');
+    nav_menu.classList.remove('opan_nav_menu');
+  })
+
+  pickup.addEventListener('click', () => {
+    header.classList.remove('open_header');
+    hunburger_menu.classList.remove('open');
+    mask_menu.classList.remove('open_mask');
+    nav_menu.classList.remove('opan_nav_menu');
+  })
+
+  contact.addEventListener('click', () => {
+    header.classList.remove('open_header');
+    hunburger_menu.classList.remove('open');
+    mask_menu.classList.remove('open_mask');
+    nav_menu.classList.remove('opan_nav_menu');
+  })
 });
